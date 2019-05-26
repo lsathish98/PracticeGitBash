0c655eb46e68d778770962f74594c02e273e034c
lsathish98 <lsathish98@gmail.com>
2019-05-26 16:26:27 +0530

    second push from branch

--- src/myPack/Firstpgm.java
+++ src/myPack/Firstpgm.java
@@ -7,6 +7,7 @@ public class Firstpgm {
 		System.out.println("first push from masters");
 		System.out.println("second push from masters");
 		System.out.println("first push from branch1");
+		System.out.println("second push from branch1");
 	}
 
 }
